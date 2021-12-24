2f1d6477d11d1584cc0cff8b0b29457efdf23cbc
Jonathan Rod <jonyrm91@gmail.com>
2021-12-24 11:24:07 +0100

    styles: basic styles

--- src/components/App.js
+++ src/components/App.js
@@ -9,19 +9,19 @@ export const App = () => {
       <h1 class=" text-cyan-600">Play with your texts!</h1>
       <div class="static">
         <hr />
-        <form class="inline-block">
+        <form class="mainForm">
           <select name="select" id="">
             <option>To CamelCase</option>
             <option>TO UPERCASE</option>
             <option>to lowercase</option>
+            <option>to Reverse</option>
           </select>
 
-          <label for="text">Text</label>
           <textarea rows="5" cols="50">
-            Write something here
+            Write something here...
           </textarea>
 
-          <button class="" type="submit">
+          <button class="convertBtn" type="submit">
             Submit
           </button>
         </form>
