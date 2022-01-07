7c739836a6a903608c54d354d9311cc50d49f913
Jonathan Rod <jonyrm91@gmail.com>
2022-01-07 11:25:33 +0100

    feat: toast notification and copy button

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React, { useState } from 'react';
 import { TextTypes } from '../helpers/textConverter';
 import { useDarkMode } from '../hooks/useDarkMode';
-/* import { lowercase, reverse, uppercase } from '../helpers/textConverter'; */
+
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import toast, { Toaster } from 'react-hot-toast';
 
 import './App.css';
 import { NavBar } from './NavBar/NavBar';
+
+const notify = () =>
+  toast('Text copied to clipboard', { position: 'bottom-right' });
 
 export const App = () => {
   const [value, setValue] = useState('');
@@ -86,8 +91,33 @@ export const App = () => {
           </div>
         </div>
       </div>
-      <div>
-        <p class="resultantText dark:text-indigo-50">{value}</p>
+
+      <div class="copy-container flex justify-center ">
+        <CopyToClipboard text={value}>
+          <p
+            onClick={notify}
+            class=" flex justify-between align-center resultantText border-2 dark:text-indigo-50 p-4"
+          >
+            {value}
+            <button class="ml-4">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 dark:text-indigo-50"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
+                />
+              </svg>
+            </button>
+            <Toaster />
+          </p>
+        </CopyToClipboard>
       </div>
     </div>
   );
