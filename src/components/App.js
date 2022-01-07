64b2db8017217e86b7ce3a9e87845ec5f85c40f9
Jonathan Rod <jonyrm91@gmail.com>
2022-01-07 13:21:13 +0100

    warnings: changed class to className due to clear warnings

--- src/components/App.js
+++ src/components/App.js
@@ -40,34 +40,34 @@ export const App = () => {
 
   useDarkMode();
   return (
-    <div class="min-h-screen dark:bg-black transition duration-500">
-      <header class="pb-40">
+    <div className="min-h-screen dark:bg-black transition duration-500">
+      <header className="pb-40">
         <NavBar />
       </header>
 
-      <div class="text-container">
+      <div className="text-container">
         <div>
-          <div class="mb-4">
+          <div className="mb-4">
             <button
-              class="selecButton bg-indigo-500 shadow-lg shadow-indigo-500/50"
+              className="selecButton bg-indigo-500 shadow-lg shadow-indigo-500/50"
               onClick={lowerCase}
             >
               lowerCase
             </button>
             <button
-              class="selecButton bg-indigo-500 shadow-lg shadow-indigo-500/50 ml-1"
+              className="selecButton bg-indigo-500 shadow-lg shadow-indigo-500/50 ml-1"
               onClick={upperCase}
             >
               UPERCASE
             </button>
             <button
-              class="selecButton bg-indigo-500 shadow-lg shadow-indigo-500/50 ml-1"
+              className="selecButton bg-indigo-500 shadow-lg shadow-indigo-500/50 ml-1"
               onClick={reverse}
             >
               REVERSE
             </button>
             <button
-              class="selecButton bg-indigo-500 shadow-lg shadow-indigo-500/50 ml-1"
+              className="selecButton bg-indigo-500 shadow-lg shadow-indigo-500/50 ml-1"
               onClick={inverseCase}
             >
               InverseCase
@@ -75,7 +75,7 @@ export const App = () => {
           </div>
 
           <textarea
-            class="textInput dark:bg-slate-600 dark:text-indigo-100"
+            className="textInput dark:bg-slate-600 dark:text-indigo-100"
             name="textarea"
             rows="10"
             cols="50"
@@ -84,21 +84,21 @@ export const App = () => {
           ></textarea>
 
           <div>
-            <button class="clearBtn mt-4" onClick={clearForm}>
+            <button className="clearBtn mt-4" onClick={clearForm}>
               Clear
             </button>
           </div>
         </div>
       </div>
 
-      <div class="copy-container flex justify-center m-8">
+      <div className="copy-container flex justify-center m-8">
         <CopyToClipboard text={value}>
           <div
-            class="flex justify-between resultantText border-2 dark:text-indigo-50 p-4"
+            className="flex justify-between resultantText border-2 dark:text-indigo-50 p-4"
             onClick={notify}
           >
             <p>{value}</p>
-            <button class="ml-4">
+            <button className="ml-4">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-6 w-6 dark:text-indigo-50"
